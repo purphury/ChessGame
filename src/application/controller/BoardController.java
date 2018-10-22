a48f507eb21f5dffa144b9507feaf9be8328ecbb
Daniel Nix <dnix7774@gmail.com>
2018-10-22 15:49:59 -0500

    Created BoardController and the necessary code to show the screen

---
+++ src/application/controller/BoardController.java
@@ -0,0 +1,17 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+public class BoardController implements Initializable{
+	@FXML GridPane board;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
--- src/application/view/Main.java
+++ src/application/view/Main.java
@@ -1,19 +1,22 @@
 package application.view;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	public static Stage stage;
 	@Override
 	public void start(Stage primaryStage) {
+		this.stage = primaryStage;
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("Board.fxml"));
+			primaryStage.setScene(new Scene(root, 500, 600));
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
