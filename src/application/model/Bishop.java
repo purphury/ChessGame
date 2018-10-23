0f3a6ca6419e9e46b259a06149cca375696dcbf5
jojo4x4 <jonathan.balraj@gmail.com>
2018-10-23 17:12:52 -0500

    just spacing tbh

--- src/application/model/Bishop.java
+++ src/application/model/Bishop.java
@@ -14,7 +14,7 @@ public class Bishop extends Piece {
 		Integer[] coord = new Integer[2];
 		//goes through the available y slots
 		int yPos, yNeg;
-		for (int i = 0; i<8 ; i++) {
+		for (int i = 0; i < 8 ; i++) {
 			coord[0] = i;
 			yPos = i - this.getX() + this.getY(); //this checks the available movement on the positive y
 			yNeg = -i + this.getX() + this.getY();//this checks the available movement on the negative y
@@ -23,7 +23,7 @@ public class Bishop extends Piece {
 				coord[1] = yPos;
 				availCoords.add(coord);
 				}
-				if (yNeg <8 && yNeg >= 0 ) {
+				if (yNeg < 8 && yNeg >= 0 ) {
 				coord[1] = yNeg;
 				availCoords.add(coord);
 				}
