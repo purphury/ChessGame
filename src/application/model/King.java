f894ccb823a8c9c1ae2b4411bf1cefab40390be6
jojo4x4 <jonathan.balraj@gmail.com>
2018-10-23 23:52:57 -0500

    got rid of int i in King.java

--- src/application/model/King.java
+++ src/application/model/King.java
@@ -14,24 +14,23 @@ public class King extends Piece
 
 	public ArrayList<Point> getAvailableMovements(int r, int c, Board board) {
 		ArrayList<Point> availCoords = new ArrayList<>();
-		int i = 1;
 		
-		if ( r - i >= 0) 
-			availCoords.add(new Point(r - i, c));
-		if ( c + i <= 7) 
-			availCoords.add(new Point(r, c + i));
-		if (r + i <= 7) 
-			availCoords.add(new Point(r + i, c));
-		if (c - i >= 0) 
-			availCoords.add(new Point(r, c - i));
-		if (r - i >= 0 && c - i >= 0) 
-			availCoords.add(new Point(r - i, c - i));
-		if (r - i >= 0 && c + i <= 7) 
-			availCoords.add(new Point(r, c + i));
-		if (r + i <= 7 && c - i >= 0) 
-			availCoords.add(new Point(r + i, c - i));
-		if (r + i <= 7 && c + i <= 7) 
-			availCoords.add(new Point(r + i, c - i));
+		if (r - 1 >= 0) 
+			availCoords.add(new Point(r - 1, c));
+		if (c + 1 <= 7) 
+			availCoords.add(new Point(r, c + 1));
+		if (r + 1 <= 7) 
+			availCoords.add(new Point(r + 1, c));
+		if (c - 1 >= 0) 
+			availCoords.add(new Point(r, c - 1));
+		if (r - 1 >= 0 && c - 1 >= 0) 
+			availCoords.add(new Point(r - 1, c - 1));
+		if (r - 1 >= 0 && c + 1 <= 7) 
+			availCoords.add(new Point(r, c + 1));
+		if (r + 1 <= 7 && c - 1 >= 0) 
+			availCoords.add(new Point(r + 1, c - 1));
+		if (r + 1 <= 7 && c + 1 <= 7) 
+			availCoords.add(new Point(r + 1, c - 1));
 		
 		return availCoords;
 	}
