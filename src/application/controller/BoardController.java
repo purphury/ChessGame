54eb0ca48f7427d2bdffc1172b4f505240b30d90
majd <majd.hamoudah@gmail.com>
2018-10-26 14:57:35 -0500

    add the images file and gave all the pics an id and declared them in the cotroller

--- src/application/controller/BoardController.java
+++ src/application/controller/BoardController.java
@@ -9,11 +9,78 @@ import application.model.Board;
 import application.model.Piece;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class BoardController implements Initializable{
 	@FXML GridPane gridBoard;
 	private Board board;
+	@FXML
+	private ImageView blackRookOne;
+	@FXML
+	private ImageView blackKnightOne;
+	@FXML
+	private ImageView blackBishopOne;
+	@FXML
+	private ImageView blackQueen;
+	@FXML
+	private ImageView blackKing;
+	@FXML
+	private ImageView blackBishopTwo;
+	@FXML
+	private ImageView blackKnightTwo;
+	@FXML
+	private ImageView blackRookTwo;
+	@FXML
+	private ImageView blackPawnOne;
+	@FXML
+	private ImageView blackPawnTwo;
+	@FXML
+	private ImageView blackPawnThree;
+	@FXML
+	private ImageView blackPawnFour;
+	@FXML
+	private ImageView blackPawnFive;
+	@FXML
+	private ImageView blackPawnSix;
+	@FXML
+	private ImageView blackPawnSeven;
+	@FXML
+	private ImageView blackPawnEight;
+	@FXML
+	private ImageView whitePawnOne;
+	@FXML
+	private ImageView whitePawnTwo;
+	@FXML
+	private ImageView whitePawnThree;
+	@FXML
+	private ImageView whitePawnFour;
+	@FXML
+	private ImageView whitePawnFive;
+	@FXML
+	private ImageView whitePawnSix;
+	@FXML
+	private ImageView whitePawnseven;
+	@FXML
+	private ImageView whitePawnEight;
+	@FXML
+	private ImageView whiteRookOne;
+	@FXML
+	private ImageView whiteKnightOne;
+	@FXML
+	private ImageView whiteBishopOne;
+	@FXML
+	private ImageView whiteQueen;
+	@FXML
+	private ImageView whiteKing;
+	@FXML
+	private ImageView whiteBishopTwo;
+	@FXML
+	private ImageView whiteKnightTwo;
+	@FXML
+	private ImageView whiteRookTwo;
+	
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
