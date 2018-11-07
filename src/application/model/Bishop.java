071eede592cb2a1f9d3daf55e87cd41b5724bbbf
jojo4x4 <jonathan.balraj@gmail.com>
2018-11-06 23:59:55 -0600

    added ally collision detection for bishop

--- src/application/model/Bishop.java
+++ src/application/model/Bishop.java
@@ -16,24 +16,43 @@ public class Bishop extends Piece {
 		for (int i = 1; i < 8 ; i++) {
 			//northwest path
 			if (!NWBlocked && r - i >= 0 && c - i >= 0) {
+				if(board.getPiece(new Coordinate(r - i, c), this.getType()) != null) { //blocked by ally
+					NWBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r - i, c - i));
 				if(board.getPiece(new Coordinate(r - i, c - i), this.otherType()) != null)
 					NWBlocked = true;
 			}
+		} for (int i = 1; i < 8 ; i++) {
 			//northeast path
 			if (!NEBlocked && r - i >= 0 && c + i <= 7) {
+				if(board.getPiece(new Coordinate(r - i, c), this.getType()) != null) { //blocked by ally
+					NEBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r - i, c + i));
 				if(board.getPiece(new Coordinate(r - i, c + i), this.otherType()) != null)
 					NEBlocked = true;
 			}
+		} for (int i = 1; i < 8 ; i++) {
 			//southwest path
 			if (!SWBlocked && r + i <= 7 && c - i >= 0) {
+				if(board.getPiece(new Coordinate(r - i, c), this.getType()) != null) { //blocked by ally
+					SWBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r + i, c - i));
 				if(board.getPiece(new Coordinate(r + i, c - i), this.otherType()) != null)
 					SWBlocked = true;
 			}
+		} for (int i = 1; i < 8 ; i++) {
 			//southeast path
 			if (!SEBlocked && r + i <= 7 && c + i <= 7) {
+				if(board.getPiece(new Coordinate(r - i, c), this.getType()) != null) { //blocked by ally
+					SEBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r + i, c + i));
 				if(board.getPiece(new Coordinate(r + i, c + i), this.otherType()) != null)
 					SEBlocked = true;
