7de59ab21c468b8252d26c6cc66c45f51764aaee
jojo4x4 <jonathan.balraj@gmail.com>
2018-11-06 22:43:00 -0600

    allies block rook

--- src/application/model/Rook.java
+++ src/application/model/Rook.java
@@ -17,24 +17,40 @@ public class Rook extends Piece
 		for (int i = 1; i < 8 ; i++){
 			//north path
 			if (!NBlocked && r - i >= 0) {
+				if(board.getPiece(new Coordinate(r - i, c), this.getType()) != null) { //blocked by ally
+					NBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r - i, c));
-				if(board.getPiece(new Coordinate(r - i, c), this.otherType()) != null)
+				if(board.getPiece(new Coordinate(r - i, c), this.otherType()) != null) //blocked by enemy
 					NBlocked = true;
 			}
 			//east path
 			if (!EBlocked && c + i <= 7) {
+				if(board.getPiece(new Coordinate(r, c + i), this.getType()) != null) { //blocked by ally
+					NBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r, c + i));
 				if(board.getPiece(new Coordinate(r, c + i), this.otherType()) != null)
 					NBlocked = true;
 			}
 			//south path
 			if (!SBlocked && r + i <= 7) {
+				if(board.getPiece(new Coordinate(r + i, c), this.getType()) != null) { //blocked by ally
+					NBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r + i, c));
 				if(board.getPiece(new Coordinate(r + i, c), this.otherType()) != null)
 					NBlocked = true;
 			}
 			//west path
 			if (!WBlocked && c - i >= 0) {
+				if(board.getPiece(new Coordinate(r, c - i), this.getType()) != null) { //blocked by ally
+					NBlocked = true;
+					continue;
+				}
 				availCoords.add(new Coordinate(r, c - i));
 				if(board.getPiece(new Coordinate(r, c - i), this.otherType()) != null)
 					NBlocked = true;
