96c33065bc6fe1e2aa64dfedfdc0d5a3d6a4a27c
jojo4x4 <jonathan.balraj@gmail.com>
2018-11-07 22:14:58 -0600

    King works now without shortcircuiting

--- src/application/model/King.java
+++ src/application/model/King.java
@@ -13,32 +13,42 @@ public class King extends Piece
 
 	public ArrayList<Coordinate> getAvailableMovements(int r, int c, Board board) {
 		ArrayList<Coordinate> availCoords = new ArrayList<>();
-//		int[][] possCoords = {{r-1,c},{r,c+1},{r+1,c},{r+1,c},{r,c-1},{r-1,c-1},{r,c+1},{r+1,c-1},{r+1,c-1}};
-//		
-//		for(int[] possCoord : possCoords){
-//			if(0 <= possCoord[0] && possCoord[0] <= 7 && 0 <= possCoord[1] && possCoord[1] <= 7)
-//				availCoords.add(new Coordinate(possCoord[0], possCoord[1]));
-//		}	
-		if (boundsChecker(r - 1, c - 1) && board.getPiece(new Coordinate(r - 1, c - 1), this.getType()) == null || board.getPiece(new Coordinate(r - 1, c - 1), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r - 1, c - 1));
-		if (boundsChecker(r - 1) && board.getPiece(new Coordinate(r - 1, c), this.getType()) == null || board.getPiece(new Coordinate(r - 1, c), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r - 1, c));
-		if (boundsChecker(r - 1, c + 1) && board.getPiece(new Coordinate(r - 1, c + 1), this.getType()) == null || board.getPiece(new Coordinate(r - 1, c + 1), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r - 1, c + 1));
-		if (boundsChecker(c - 1) && board.getPiece(new Coordinate(r, c - 1), this.getType()) == null || board.getPiece(new Coordinate(r, c - 1), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r, c - 1));
-		if (boundsChecker(c + 1) && board.getPiece(new Coordinate(r, c + 1), this.getType()) == null || board.getPiece(new Coordinate(r, c + 1), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r, c + 1));
-		if ((boundsChecker(r + 1, c - 1) && board.getPiece(new Coordinate(r + 1, c - 1), this.getType()) == null) || board.getPiece(new Coordinate(r + 1, c - 1), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r + 1, c - 1));
-		if (boundsChecker(r + 1)  && board.getPiece(new Coordinate(r + 1, c), this.getType()) == null || board.getPiece(new Coordinate(r + 1, c), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r + 1, c));
-		if (boundsChecker(r + 1, c + 1) && board.getPiece(new Coordinate(r + 1, c + 1), this.getType()) == null || board.getPiece(new Coordinate(r + 1, c + 1), this.otherType()) != null) 
-			availCoords.add(new Coordinate(r + 1, c + 1));
-		
+		int[][] possCoords = {{r-1,c},{r,c+1},{r+1,c},{r+1,c},{r,c-1},{r-1,c-1},{r,c+1},{r+1,c-1},{r+1,c-1}};
+
+		for(int i = 0; i < 8; i++) {
+			if (boundsChecker(possCoords[i][0], possCoords[i][1])) 
+				if(board.getPiece(new Coordinate(possCoords[i][0], possCoords[i][1]), this.getType()) == null || 
+				board.getPiece(new Coordinate(possCoords[i][0], possCoords[i][1]), this.otherType()) != null) 
+					availCoords.add(new Coordinate(possCoords[i][0], possCoords[i][1]));
+		}
+
+		//		if (boundsChecker(r - 1, c - 1)) 
+		//			if(board.getPiece(new Coordinate(r - 1, c - 1), this.getType()) == null || board.getPiece(new Coordinate(r - 1, c - 1), this.otherType()) != null) 
+		//				availCoords.add(new Coordinate(r - 1, c - 1));
+		//		if (boundsChecker(r - 1)) if(board.getPiece(new Coordinate(r - 1, c), this.getType()) == null || board.getPiece(new Coordinate(r - 1, c), this.otherType()) != null) 
+		//			availCoords.add(new Coordinate(r - 1, c));
+		//		if (boundsChecker(r - 1, c + 1)) 
+		//			if(board.getPiece(new Coordinate(r - 1, c + 1), this.getType()) == null || board.getPiece(new Coordinate(r - 1, c + 1), this.otherType()) != null) 
+		//				availCoords.add(new Coordinate(r - 1, c + 1));
+		//		if (boundsChecker(c - 1)) 
+		//			if(board.getPiece(new Coordinate(r, c - 1), this.getType()) == null || board.getPiece(new Coordinate(r, c - 1), this.otherType()) != null) 
+		//				availCoords.add(new Coordinate(r, c - 1));
+		//		if (boundsChecker(c + 1)) 
+		//			if(board.getPiece(new Coordinate(r, c + 1), this.getType()) == null || board.getPiece(new Coordinate(r, c + 1), this.otherType()) != null) 
+		//				availCoords.add(new Coordinate(r, c + 1));
+		//		if (boundsChecker(r + 1, c - 1)) 
+		//			if(board.getPiece(new Coordinate(r + 1, c - 1), this.getType()) == null || board.getPiece(new Coordinate(r + 1, c - 1), this.otherType()) != null) 
+		//				availCoords.add(new Coordinate(r + 1, c - 1));
+		//		if (boundsChecker(r + 1))
+		//			if(board.getPiece(new Coordinate(r + 1, c), this.getType()) == null || board.getPiece(new Coordinate(r + 1, c), this.otherType()) != null) 
+		//				availCoords.add(new Coordinate(r + 1, c));
+		//		if (boundsChecker(r + 1, c + 1))
+		//			if(board.getPiece(new Coordinate(r + 1, c + 1), this.getType()) == null || board.getPiece(new Coordinate(r + 1, c + 1), this.otherType()) != null) 
+		//				availCoords.add(new Coordinate(r + 1, c + 1));
+
 		return availCoords;
 	}
-	
+
 	public String toString() {
 		return "K";
 	}
