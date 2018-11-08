0ea823da887b907826837d22b455819e9ce4a391
Chris Crabtree <chris414862@chriss-MacBook-Pro-2.local>
2018-11-08 14:50:00 -0600

    King wasnt working so I fixed that as well

--- src/application/model/King.java
+++ src/application/model/King.java
@@ -4,27 +4,36 @@ import java.util.ArrayList;
 
 import application.model.Board.Type;
 
-public class King extends Piece
-{
-	public King(Type color) 
-	{
+public class King extends Piece {
+	public King(Type color) {
 		super(color);
 	}
 
 	public ArrayList<Coordinate> getAvailableMovements(int r, int c, Board board) {
 		ArrayList<Coordinate> availCoords = new ArrayList<>();
-		int[][] possCoords = {{r-1,c},{r,c+1},{r+1,c},{r+1,c},{r,c-1},{r-1,c-1},{r,c+1},{r+1,c-1},{r+1,c-1}};
+		//first number is the vertical direction, the second is the horizontal direction
+		//negative is up or left, positive is down or right. Zero keeps it stationary
+		addMovement(availCoords, r - 1, c, board);// South direction
+		addMovement(availCoords, r, c - 1, board);// West direction
+		addMovement(availCoords, r + 1, c, board);// North direction
+		addMovement(availCoords, r, c + 1, board);// East direction
+		addMovement(availCoords, r - 1, c - 1, board);// Northwest direction
+		addMovement(availCoords, r - 1, c + 1, board);// Northeast direction
+		addMovement(availCoords, r + 1, c - 1, board);// Southwest direction
+		addMovement(availCoords, r + 1, c + 1, board);// Southeast direction}
 
-		for(int i = 0; i < 8; i++) {
-			if (boundsChecker(possCoords[i][0], possCoords[i][1])) 
-				if(board.getPiece(possCoords[i][0], possCoords[i][1], this.getType()) == null || board.getPiece(possCoords[i][0], possCoords[i][1], this.otherType()) != null) 
-					availCoords.add(new Coordinate(possCoords[i][0], possCoords[i][1]));
-		}
-		
 		return availCoords;
 	}
 
-	public String toString() {
-		return "K";
+	public void addMovement(ArrayList<Coordinate> availCoords, int r, int c, Board board) {
+		if (boundsChecker(r, c)) {
+			if (board.hasPiece(new Coordinate(r, c))) {// a piece is on this location
+				if (board.getPiece(new Coordinate(r, c)).getType() == this.otherType())// its an enemy piece
+					availCoords.add(new Coordinate(r, c));
+			} 
+			else // This location is a free space
+				availCoords.add(new Coordinate(r, c));
+		}
 	}
+
 }
--- src/application/model/Knight.java
+++ src/application/model/Knight.java
@@ -17,26 +17,28 @@ public class Knight extends Piece
 		ArrayList<Coordinate> availCoords = new ArrayList<>();
 		
 		// two squares away horizontally and one square vertically		
-		if(boundsChecker(r + 1, c + 2)) addMovement(availCoords,r + 1, c + 2, board);	
-		if(boundsChecker(r - 1, c + 2)) addMovement(availCoords,r - 1, c + 2, board);
-		if(boundsChecker(r + 1, c - 2)) addMovement(availCoords,r + 1, c - 2, board);
-		if(boundsChecker(r - 1, c - 2)) addMovement(availCoords,r - 1, c - 2, board);
+		addMovement(availCoords,r + 1, c + 2, board);	
+		addMovement(availCoords,r - 1, c + 2, board);
+		addMovement(availCoords,r + 1, c - 2, board);
+		addMovement(availCoords,r - 1, c - 2, board);
 		
 		//two squares vertically and one square horizontally
-		if(boundsChecker(r + 2, c + 1)) addMovement(availCoords,r + 2, c + 1, board);
-		if(boundsChecker(r + 2, c - 1)) addMovement(availCoords,r + 2, c - 1, board);
-		if(boundsChecker(r - 2, c - 1)) addMovement(availCoords,r - 2, c - 1, board);
-		if(boundsChecker(r - 2, c + 1)) addMovement(availCoords,r - 2, c + 1, board);
+		addMovement(availCoords,r + 2, c + 1, board);
+		addMovement(availCoords,r + 2, c - 1, board);
+		addMovement(availCoords,r - 2, c - 1, board);
+		addMovement(availCoords,r - 2, c + 1, board);
 		
 		return availCoords;
 	}
 	
 	public void addMovement(ArrayList<Coordinate> availCoords,int r, int c, Board board) {
-		if(board.hasPiece(new Coordinate(r,c))) {//a piece is on this location
-			if(board.getPiece(new Coordinate(r,c)).getType() == this.otherType())//its an enemy piece
+		if (boundsChecker(r, c)) {
+			if(board.hasPiece(new Coordinate(r,c))) {//a piece is on this location
+				if(board.getPiece(new Coordinate(r,c)).getType() == this.otherType())//its an enemy piece
+					availCoords.add(new Coordinate(r, c));
+			}
+			else	//This location is a free space	
 				availCoords.add(new Coordinate(r, c));
-		}
-		else	//This location is a free space	
-		availCoords.add(new Coordinate(r, c));
+	}
 	}
 }
