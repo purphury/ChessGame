fb7c8c42a329dc89a61c8b94e26c018fb41a9a23
majd <majd.hamoudah@gmail.com>
2018-11-22 12:48:29 -0600

    fixed bugs in game

--- src/application/controller/BoardController.java
+++ src/application/controller/BoardController.java
@@ -111,26 +111,28 @@ public class BoardController {
 					if(typeOfMove == 3) {					
 						promotePawn(boardModel.getPreviousTurn(), c);
 					}
-				}
-				//moving rook
-				if (typeOfMove == 5) {
-					Pane p = (Pane) getPaneByRowColumnIndex(c.getRowIndex(), 7);
-					if(p.getChildren().get(0) != null) {
-						ImageView thing = (ImageView) p.getChildren().get(0);
-						p.getChildren().remove(thing);
-						Pane destination = (Pane) getPaneByRowColumnIndex(c.getRowIndex(),  c.getColumnIndex() - 1);
-						destination.getChildren().add(thing);
-					}
 					
-				}
-				if (typeOfMove == 4) {
-					Pane p = (Pane) getPaneByRowColumnIndex(c.getRowIndex(), 0);
-					if(p.getChildren().get(0) != null) {
-						ImageView thing = (ImageView) p.getChildren().get(0);
-						p.getChildren().remove(thing);
-						Pane destination = (Pane) getPaneByRowColumnIndex(c.getRowIndex(),  c.getColumnIndex() + 1);
-						destination.getChildren().add(thing);
+					//moving rook
+					if (typeOfMove == 5) {
+						Pane p = (Pane) getPaneByRowColumnIndex(c.getRowIndex(), 7);
+						if(p.getChildren().get(0) != null) {
+							ImageView thing = (ImageView) p.getChildren().get(0);
+							p.getChildren().remove(thing);
+							Pane destination = (Pane) getPaneByRowColumnIndex(c.getRowIndex(),  c.getColumnIndex() - 1);
+							destination.getChildren().add(thing);
+						}
+						
 					}
+					if (typeOfMove == 4) {
+						Pane p = (Pane) getPaneByRowColumnIndex(c.getRowIndex(), 0);
+						if(p.getChildren().get(0) != null) {
+							ImageView thing = (ImageView) p.getChildren().get(0);
+							p.getChildren().remove(thing);
+							Pane destination = (Pane) getPaneByRowColumnIndex(c.getRowIndex(),  c.getColumnIndex() + 1);
+							destination.getChildren().add(thing);
+							}
+					}
+				
 					
 				}
 				
