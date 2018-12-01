015ab824b2bd76e9e821de0090d3481b5b94806a
daniel-nix <dnix7774@gmail.com>
2018-12-01 11:29:13 -0600

    Created strength boards for the pieces.

--- src/application/model/AI.java
+++ src/application/model/AI.java
@@ -1,5 +1,21 @@
 package application.model;
 
 public class AI {
-
+	private double[][] KingStrength;
+	private double[][] QueenStrength;
+	private double[][] RookStrength;
+	private double[][] BishopStrength;
+	private double[][] KnightStrength;
+	private double[][] PawnStrength;
+	public AI() {
+		KingStrength = KingStrengthBoard.KingStrengthBoard;
+		QueenStrength = QueenStrengthBoard.QueenStrengthBoard;
+		RookStrength = RookStrengthBoard.RookStrengthBoard;
+		BishopStrength = BishopStrengthBoard.BishopStrengthBoard;
+		KnightStrength = KnightStrengthBoard.KnightStrengthBoard;
+		PawnStrength = PawnStrengthBoard.PawnStrengthBoard;
+	}
+	public int evaluateBoard(Board board) {
+		return 0;
+	}
 }
