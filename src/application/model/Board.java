b12770fff722d17e46b763094ca0ee8e6e6ddead
jojo4x4 <jonathan.balraj@gmail.com>
2018-12-01 14:43:14 -0600

    added display method

--- src/application/model/Board.java
+++ src/application/model/Board.java
@@ -84,6 +84,20 @@ public class Board {
 		this.previousBoard = null;
 	}
 	
+	public void display() {
+		for(int i = 0; i< 8 ; i++) {
+			for(int j = 0; i < 8 ; j++) {
+				System.out.print("| ");
+				if(this.hasPiece(i,j)) {
+					System.out.print(this.getPiece(i,j)+" ");
+				}
+				else
+					System.out.println(" ");
+			}
+			System.out.println();
+		}
+	}
+	
 	public String getWhiteName() {
 		return whiteName;
 	}
